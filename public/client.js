3a8d50cbe3c411906a37feb6ed7c9ee841a3332c
System <none>
2017-10-09 22:06:36 +0000

    🌗🏹 Updated with Glitch - changed for loops to forEach

--- public/client.js
+++ public/client.js
@@ -20,23 +20,38 @@ var todoList = {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
     
-    // Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
-        completedTodos++;
-      }
-    }
+    // // Get number of completed todos.
+    // for (var i = 0; i < totalTodos; i++) {
+    //   if (this.todos[i].completed === true) {
+    //     completedTodos++;
+    //   }
+    // }
+    
+    this.todos.forEach(function(todo){
+      if (todo.completed === true) {
+         completedTodos++;
+       }
+    });
+    
     
     // Case 1: If everything’s true, make everything false.
     if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
-      }
+      // for (var i = 0; i < totalTodos; i++) {
+      //   this.todos[i].completed = false;
+      // }
+      
+      this.todos.forEach(function(todo){
+        todo.completed = false;
+      })
     // Case 2: Otherwise, make everything true.
     } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }      
+      // for (var i = 0; i < totalTodos; i++) {
+      //   this.todos[i].completed = true;
+      // }      
+      
+      this.todos.forEach(function(todo){
+        todo.completed = true;
+      });
     }
   }
 };
