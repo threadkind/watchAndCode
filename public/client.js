bcb083181dccc40455d9d935b775fde1398c744d
System <none>
2017-10-10 23:50:11 +0000

    🚧📫 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -94,9 +94,24 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
+//     for (var i = 0; i < todoList.todos.length; i++) {
+//       var todoLi = document.createElement('li');
+//       var todo = todoList.todos[i];
+//       var todoTextWithCompletion = '';
+
+//       if (todo.completed === true) {
+//         todoTextWithCompletion = '(x) ' + todo.todoText;
+//       } else {
+//         todoTextWithCompletion = '( ) ' + todo.todoText;
+//       }
+//       todoLi.id = i;
+//       todoLi.textContent = todoTextWithCompletion;
+//       todoLi.appendChild(this.createDeleteButton());
+//       todosUl.appendChild(todoLi);
+//     }
+    
+    todoList.todos.forEach(function(todo, position){
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
@@ -104,11 +119,11 @@ var view = {
       } else {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
-      todosUl.appendChild(todoLi);
-    }
+      todosUl.appendChild(todoLi);        
+                           }, this);
   },
   createDeleteButton: function(){
     var deleteButton = document.createElement('button');
